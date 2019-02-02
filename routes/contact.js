096e300df2f49ab84cc3372da492a17c6588dda6
chandni <chandptl.91@gmail.com>
2019-02-02 11:02:45 -0500

    Conntected with mongoDB

---
+++ routes/contact.js
@@ -0,0 +1,10 @@
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+/* GET contact list page - READ */
+router.get('/',(req,res,next) => {
+
+});
+
+module.exports = router;
