524a040fcce51ee236e98098b3203f471086fc6f
chandni <chandptl.91@gmail.com>
2019-02-02 11:34:47 -0500

    Seperated Heder & Footer

--- routes/contact.js
+++ routes/contact.js
@@ -2,9 +2,24 @@ let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
 
+//create reference to the contact model
+let contact  = require('../model/contact');
+
 /* GET contact list page - READ */
 router.get('/',(req,res,next) => {
-
+    contact.find((err, contactList) => {
+        //console.log(contactList);
+        if(err){
+            return console.error(err);
+        }
+        else{
+            console.log(contactList);
+            /*res.render('contacts/index', {
+                title:'Contact List',
+                contactList:contactList
+            });*/
+        }
+    })
 });
 
 module.exports = router;
